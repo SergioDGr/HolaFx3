f3e2e3423e3980aaeafe2f5185fa72e9cb53177b
SergioDGr <sergiodanielgroppa@gmail.com>
2023-09-20 11:46:05 +0200

    [add] Estructura Base
    Creamos la estructura base del proyecto javaFx

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    	
+    }
+    
+}
