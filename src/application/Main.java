0654015425e2e7ba1e62d4932b31fe9d5e1c7aad
SergioDGr <sergiodanielgroppa@gmail.com>
2023-09-20 11:59:54 +0200

    [add] Ventana Inicial
    Añadido una ventana con estructura base

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -24,6 +25,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+    	//
+    	Text mensaje = new Text("Introduzca saludo:");
+    	TextField inputFrase = new TextField();
+    	Button btnSaludo = new Button("Hola");
+    	btnSaludo.setOnAction(e -> saludar());
+    	//
+    	VBox box = new VBox();
+    	box.getChildren().add(mensaje);
+    	box.getChildren().add(inputFrase);
+    	box.getChildren().add(btnSaludo);
+    	//
+    	Scene scene = new Scene(box, 400, 400);
+        stage.setScene(scene);
+        stage.setTitle("Ventana");
+        stage.show();
+    }
+    
+    private void saludar() {
     	
     }
     
